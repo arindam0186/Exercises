59cd869c913872f61459bf9c594d00c136b4f557
arindam0186 <arindam.goel00@gmail.com>
2020-09-15 18:31:19 +0530

    BloodDonor class update

--- src/Day1_Exercise/BloodDonor.java
+++ src/Day1_Exercise/BloodDonor.java
@@ -2,4 +2,44 @@ package Day1_Exercise;
 
 public class BloodDonor {
 
+	private String name;
+	private int age;
+	private String gender;
+	private String address;
+	public BloodDonor() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public BloodDonor(String name, int age, String gender, String address) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.gender = gender;
+		this.address = address;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
 }

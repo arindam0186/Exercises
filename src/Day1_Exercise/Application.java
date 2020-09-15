bb6443dd2b49669b343a57311e3a36d7970d4bca
arindam0186 <arindam.goel00@gmail.com>
2020-09-15 18:48:56 +0530

    Application class update

--- src/Day1_Exercise/Application.java
+++ src/Day1_Exercise/Application.java
@@ -5,6 +5,19 @@ public class Application {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		BloodDonor first = new BloodDonor("Harry",25,"Male","Boston");
+		
+		System.out.println(first.getName());
+		System.out.println(first.getAddress());
+		
+		BloodDonor second = new BloodDonor();
+		
+		second.setName("Selena");
+		second.setAddress("California");
+		
+		System.out.println(second.getName());
+		System.out.println(second.getAddress());
+		
 	}
 
 }
